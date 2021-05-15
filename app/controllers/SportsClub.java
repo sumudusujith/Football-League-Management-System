597dd8027a0d5bb5ad234c0a5d9ea4159dac7847
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-15 23:58:10 +0530

    sc com1

---
+++ app/controllers/SportsClub.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class SportsClub implements Serializable { //ai
+    private String club_name;
+    private String location_club;
+    public SportsClub(){
+
+    }
